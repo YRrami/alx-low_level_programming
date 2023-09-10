7bf7a323badaf6e203f75189f51707f2f57f53c8
YRrami <johnjohn444465@gmail.com>
2023-09-10 14:26:29 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ *print_rev - returns the string in reverse
+ *@s: char
+ *
+ */
+void print_rev(char *s)
+{
+	int X;
+
+	for (x = '\0'; str[x] = 0 ; s--)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
