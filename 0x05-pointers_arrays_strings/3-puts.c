4f5d9c1203baf1ae320ba979ee84ff7ef52eef9f
YRrami <johnjohn444465@gmail.com>
2023-09-10 08:17:54 -0700

    ws

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+ *_puts - print string to stdout
+ *@str: pointer to string
+ */
+void _puts(char *str)
+{
+	int x;
+
+	x = 0;
+	while (str[x] != \0)
+	{
+		x++;
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
