9ba9de4f5e83e5b0e46dc4a3ef1fc191f55bf2bd
YRrami <johnjohn444465@gmail.com>
2023-09-10 08:05:37 -0700

    s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+ * _strlen - return the length of a string
+ * @s: char
+ *
+ * Return: the numbers integer
+ */
+int _strlen(char *s)
+{
+	int numbers
+
+	for (numbers = 0; *s > '\0'; s++)
+
+	++numbers;
+	return (numbers);
+
+}
