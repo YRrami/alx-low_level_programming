6c0baaad8480ac941ecab336399a7b602165c3db
YRrami <johnjohn444465@gmail.com>
2023-09-12 19:02:56 -0700

    s

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *factorial - factorial of a number
+ *@n: integer
+ *Return: 1
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return ("-1");
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (1);
+	}
+
+
+}
