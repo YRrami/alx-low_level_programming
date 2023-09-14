a4dd3ec74156a9a4eee721d3da896f03398a327b
YRrami <johnjohn444465@gmail.com>
2023-09-14 16:31:18 -0700

    s

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ *is_prime_number - func
+ *@n : string
+ *Return: the val
+ */
+int is_prime_number(int n)
+{
+	int r;
+
+	r = r / 2;
+	if (r + r == n || n % 10 == 5)
+	{
+		return (0);
+	}
+	else if (n <= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
