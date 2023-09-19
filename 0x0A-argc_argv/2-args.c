914e805ab7ccbbcc6523325d6776bb6ce425cd2c
YRrami <johnjohn444465@gmail.com>
2023-09-18 22:52:10 -0700

    s

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ *main -func
+ *@argc: dwad
+ *@argv:ss
+ *Return: ddwa
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+}
