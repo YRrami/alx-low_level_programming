c8b00b6ca564af5c3334e32d18b37fd842d51990
YRrami <johnjohn444465@gmail.com>
2023-09-18 21:35:57 -0700

    s

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ *main - func
+ *@argc: int
+ *@argv: test
+ *Return: void
+ */
+int main(int argc, char *argv[])
+{
+		printf("%s \n", argv[0]);
+}
