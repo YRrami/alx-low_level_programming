49062df29af17fc33d26a2712187df68b6d10e93
YRrami <johnjohn444465@gmail.com>
2023-09-23 18:17:12 -0700

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+
+char *_strdup(char *str)
+{
+	int l = 0;
+	char *p;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (l < *str)
+	{
+	 l++;
+	}
+	p = malloc(sizeof(char) * (l + 1));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (int x = 0; x <= *str; x++)
+	p[x] = str[x];
+	
+	return (p);
+}
