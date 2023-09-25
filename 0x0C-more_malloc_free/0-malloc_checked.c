e0952c49a831df5101d7b54e73dec33dc46688bf
YRrami <johnjohn444465@gmail.com>
2023-09-24 20:56:45 -0700

    s

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * malloc_checked - func
- * @b: unsigned int
- * Return: x
+ *malloc_checked - func
+ *@b: unsigned int
+ *Return: x
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *x;
+	unsigned int *x;
 
 	x = malloc(b);
 	if (x == NULL)
