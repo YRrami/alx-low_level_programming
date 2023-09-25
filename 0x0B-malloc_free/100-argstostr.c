edea816c612ef8f0ec5fbf2bc662bacf46dc3572
YRrami <johnjohn444465@gmail.com>
2023-09-24 19:35:46 -0700

    s

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+ *argstostr - hello
+ *@ac: hello
+ *@av: hello
+ *Return: hello
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int x,y,z;
+	char *res, *mod;
+
+	if (ac == 0)
+	{
+		return (NULL);
+	}
+	if (av == NULL)
+	{
+		return (NULL);
+	}
+
+		while (x < ac)
+		{
+			mod = av[x];
+			while (*mod)
+			{
+				z++;
+				mod++;
+			}
+			z++;
+			mod++;
+		}
+		res = malloc(z + 1);
+		i = 0;
+			while (x < ac)
+			{
+				mod = av[x];
+				while (*new)
+				{
+					res[j] = *new;
+					new++;
+					y++
+				}
+				res[j]= '\n';
+				j++;
+				i++;
+			}
+			res[z + 1] = '\0';
+			return (res);
+
+}
