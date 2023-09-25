7906ed070f738cc6e460937bcd9c3d65e6f36b8a
YRrami <johnjohn444465@gmail.com>
2023-09-24 21:40:27 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,34 +13,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1 = 0, len2 = 0, k;
-char *ptr;
+	unsigned int len1 = 0, len2 = 0, k;
+	char *ptr;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-while (s1[len1])
-{
-	len1++;
-}
-while (s2[len2])
-{
-	len2++;
-}
-if (n >= len2++)
-n = len2;
-ptr = malloc(sizeof(char) * (len1 + n + 1));
-if (ptr == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1])
+	{
+		len1++;
+	}
+	while (s2[len2])
+	{
+		len2++;
+	}
+	if (n >= len2)
+	n = len2;
+	len2 = 0;
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
 	return (NULL);
-for (k = 0; k < len1; k++)
-{
-	ptr[k] == s1[k];
-}
-for (; k < (len1 + n) ; k++)
-{
-	ptr[k] = s2[k - len1];
-}
-ptr[k] = '\0';
-return (ptr);
+	for (k = 0; k < len1; k++)
+	{
+		ptr[k] == s1[k];
+	}
+	for (; k < (len1 + n) ; k++)
+	{
+		ptr[k] = s2[k - len1];
+	}
+	ptr[k] = '\0';
+	return (ptr);
 }
