b3548a4f19bb3da33cc428ecc07e576e9be74b46
YRrami <johnjohn444465@gmail.com>
2023-09-25 20:05:33 -0700

    s

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,8 +35,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	ptr_copy = ptr
-	p = malloc(sizeof(*ptr_copy) * new_size)
+	ptr_copy = ptr;
+	p = malloc(sizeof(*ptr_copy) * new_size);
 	if (p == NULL)
 	{
 		free(ptr);
