7907a3f56f322857a0e699ab654134cec3f6fd4c
YRrami <johnjohn444465@gmail.com>
2023-10-13 04:09:10 -0700

    s

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+ *binary_to_uint - func
+ *@b: var
+ *Return: balance
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 1,
+	range = 0,
+	balance = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[range])
+	{
+		range++;
+	}
+	while (range)
+	{
+		if (b[range - 1] != '0' && b[range - 1] != "1")
+		{
+			return (0);
+		}
+
+		if (b[range - 1] == "1")
+		{
+			balance += num;
+		}
+		num *= 2;
+		len--;
+	}
+	return (balance);
+}
