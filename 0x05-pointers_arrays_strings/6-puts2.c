d0c056b761802c7df7dfc527cc7d880f4e5d83ee
YRrami <johnjohn444465@gmail.com>
2024-05-09 12:03:07 -0700

    s

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * puts2 - prints one char out of two
+ * @str: string
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int x;
+	while (str[length] != '\0')
+	{
+		length = length + 1;
+	}
+	for (x = 0; x < length; x += 2)
+		_putchar(str[x]);
+
+	_putchar('\n');
+}
