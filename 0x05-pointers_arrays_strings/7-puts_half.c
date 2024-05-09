e33b761dce9050d4b675428cf3f083b8dd8f3419
YRrami <johnjohn444465@gmail.com>
2024-05-09 12:13:58 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of str
+ * @str: string var
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int x;
+	int y;
+
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 == 0)
+	{
+		for (y = length / 2; str[i] != '\0'; y++)
+		{
+			_putchar(str[y]);
+		}
+	}
+	else if (length % 2)
+	{
+		for (x = (length - 1) / 2; x < length - 1; x++)
+		{
+			_putchar(str[x + 1]);
+		}
+	}
+	_putchar('\n');
+}
